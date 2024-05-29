0ea98d2ef7fbdb917311875d53c1008c824397fd
marly2024 <marlyferreira116@gmail.com>
2024-05-29 14:45:59 +0200

    sentencia for clasica

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -28,5 +28,41 @@ public class App
     	//VARIANTE # 2.Implica utilizar una sentencia de control de flujo. para recorrer
     	// el array 
     	// lo veremos en la rama "sentenciasDeControlDeFlujo"
+    	
+    	// Primero Con una sentencia for de toda la vida, o clasica
+    	
+    	
+    	/*Ejercicio # 1. solamente mostrar los nombres del array de nombres que tienen
+    	 * mas de 4 caracteres*/
+    	for(int i = 0; i < nombres.length; i++ ) {
+    		if (nombres[i].length() > 4) {
+    		System.out.println(nombres[i]);	
+    		} 
+    		
+    		
+    	}
+    	/*Que diferencia hay en utilizar el operador de autoincremento,o autodecremento,
+    	 * antes o despues
+    	 * 
+    	 * para solucionar este problema,clasico de cuando uno empieza con un lenguaje
+    	 * de programacion utilizamos JSHELL, que es como shell que es como un shell de python
+    	 * que antes Java antes o despues
+    	 * 
+    	 * El operador de autoincremento o autodecremento si esta solo en una sentencia,
+    	 * da igual que vaya antes o despues, es decir, i++; o ++i; SON EXACTAMENTE LO
+    	 * MISMO, sin embargo, i + ++] no es lo mismo que i + j++*/
+    	
+    	 /*NOTA IMPORTANTE cuando se utiliza el for clasico?
+    	  * cuando nos interesa el indice. por ejemplo, si queremos recorrer el array de numeros 
+    	  * y solamente quiero mostrar los elementos que son de indice par*/
+    	
+    	for(int i = 0; i < numeros.length; i++) {
+    	if (i % 2 == 0)
+    		System.out.println("El elemento de indice" + i + ",es par");
+    	
+    		
+    	// SEGUNDO: con un for mejorado, en ocasiones MAL llamado por each (po cada)
+    	// digo mal llamado porque la sentencia forEach existe,como veran posteriormente
     }
+  }
 }
